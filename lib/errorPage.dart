c66306130c5d942750bfb3aa58ff710315071a7d
Killian DROULEZ <killian.droulez@outlook.fr>
2022-01-12 20:49:57 +0100

    🐛 fix video not playing at beginning

--- lib/errorPage.dart
+++ lib/errorPage.dart
@@ -1,5 +1,6 @@
 import 'package:activity_display/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 class ErrorPage extends StatefulWidget {
@@ -14,14 +15,15 @@ class _ErrorPageState extends State<ErrorPage> {
 
   @override
   void initState() {
-    _videoController =
-        VideoPlayerController.asset('assets/videos/erreur_palm.mp4');
-    _videoController
-        .initialize()
-        .then((value) => {_videoController.play(), setState(() {})});
-    _videoController.addListener(checkIfVideoEnded);
-
     super.initState();
+    _videoController =
+        VideoPlayerController.asset('assets/videos/erreur_palm.mp4')
+          ..initialize().then((_) => {
+                _videoController.addListener(checkIfVideoEnded),
+                _videoController.setVolume(
+                    0), // Due to bug on web, we need to set volume to 0 before playing
+                setState(() {}),
+              });
   }
 
   @override
@@ -32,12 +34,25 @@ class _ErrorPageState extends State<ErrorPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: _videoController.value.aspectRatio,
-          child: VideoPlayer(_videoController),
+    return RawKeyboardListener(
+      autofocus: true,
+      focusNode: FocusNode(),
+      onKey: (value) {
+        // Launch error video when "Space" is pressed
+        if (value.isKeyPressed(LogicalKeyboardKey.space) ||
+            value.isKeyPressed(LogicalKeyboardKey.enter)) {
+          _videoController.play();
+          setState(() {});
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: _videoController.value.isInitialized
+              ? AspectRatio(
+                  aspectRatio: _videoController.value.aspectRatio,
+                  child: VideoPlayer(_videoController))
+              : Container(),
         ),
       ),
     );
