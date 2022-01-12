88d6ac6b25252e5746ddb6b5f491607b5574129e
Killian DROULEZ <killian.droulez@outlook.fr>
2022-01-12 20:48:45 +0100

    ♻️ improve ws management

--- lib/services/ws.dart
+++ lib/services/ws.dart
@@ -1,28 +1,45 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
-import 'package:web_socket_channel/status.dart';
 
-/// 1. Être sur un réseau wifi
-/// 2. Donner l'adresse IP + port (+ potentiel endpoint)
 class WebSocketManager {
-  static const wsName = "ws://192.168.4.1/ws";
+  static final WebSocketManager _instance = WebSocketManager._init();
+
+  factory WebSocketManager() {
+    return _instance;
+  }
+  WebSocketManager._init() {
+    initWS();
+  }
+
+  //static const wsName = "ws://192.168.4.1/ws";
+  static const wsName = "wss://echo.websocket.org";
   late WebSocketChannel channel;
+  Function(String data)? listenerCallback;
 
-  WebSocketManager() {
-    // channel is null until ws is connected
-    channel = WebSocketChannel.connect(Uri.parse(wsName));
+  int reconnectionAttempt = 0;
+
+  void resgisterListener(Function(String data) callback) {
+    listenerCallback = callback;
   }
 
-  void reconnect() {
+  void initWS() {
     channel = WebSocketChannel.connect(Uri.parse(wsName));
+    listen();
   }
 
-  void listen({required Function(String data) didRecieve}) async {
+  void listen() {
     channel.stream.listen((message) {
-      print(message);
-      didRecieve(message);
+      listenerCallback!(message);
     }, onDone: () {
-      print("FROM IMPLEM: WS Closed");
-      reconnect();
+      print("[WSManager]: WS Closed");
+      if (reconnectionAttempt < 2) {
+        reconnectionAttempt++;
+        print("[WSManager]: " +
+            reconnectionAttempt.toString() +
+            " Reconnection attempt...");
+        initWS();
+      }
+    }, onError: (err) {
+      print("[WSManager] - Error: " + err.toString());
     });
   }
 }
